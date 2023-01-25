77caf0ff229762c5bb02bc762b0d2e6b76caefb7
crashdev226 <midashand44@gmail.com>
2023-01-25 15:27:18 +0200

    chore: add Toggle

---
+++ src/components/Toggle.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	export let checked: boolean;
+</script>
+
+<button class="toggle" on:click={() => (checked = !checked)}>
+	<span class="label">
+		<slot />
+	</span>
+
+	<div class="switch" class:checked />
+</button>
+
+<style lang="scss">
+	.toggle {
+		$height: 20px;
+		$width: 48px;
+		$padding: 4px;
+
+		cursor: pointer;
+		display: inline-flex;
+		align-items: center;
+
+		background: none;
+		border: none;
+		padding: 0;
+
+		.label {
+			margin-right: 16px;
+			color: white;
+		}
+
+		.switch {
+			display: inline-block;
+			background: #ccc;
+			border-radius: 16px;
+			width: $width;
+			height: $height;
+			position: relative;
+			vertical-align: middle;
+			transition: background 0.25s;
+
+			&:before,
+			&:after {
+				content: "";
+			}
+
+			&:before {
+				$size: calc($height - 2 * $padding);
+
+				display: block;
+				background: linear-gradient(to bottom, #fff 0%, #eee 100%);
+				border-radius: 50%;
+				width: $size;
+				height: $size;
+				position: absolute;
+				top: $padding;
+				left: $padding;
+				transition: left 0.25s;
+			}
+
+			&.checked {
+				background: #56c080;
+
+				&:before {
+					left: calc($width - $height + $padding);
+				}
+			}
+
+			&:hover {
+				.switch {
+					&:before {
+						background: linear-gradient(
+							to bottom,
+							#fff 0%,
+							#fff 100%
+						);
+					}
+				}
+			}
+		}
+	}
+</style>
--- src/components/root/Popup.svelte
+++ src/components/root/Popup.svelte
@@ -1,19 +1,37 @@
 <script lang="ts">
-	console.log("Hello from popup");
+	import Toggle from "$components/Toggle.svelte";
+
+	let checked = true
+
+	$: console.log(checked)
 </script>
 
+<div class="popup">
 
-what
-<h1>
-	<img src="/icons/gear.svg" alt="Settings icon" /> TruthSense настройки
-</h1>
+	<h1>
+		<img src="/icons/gear.svg" alt="Settings icon" /> TruthSense настройки
+	</h1>
 
-Run on Facebook:
+	<Toggle bind:checked>
+		Включи във Facebook
+	</Toggle>
+</div>
 
-<label class="toggle">
-	<input class="toggle-checkbox" type="checkbox" checked />
-	<div class="toggle-switch" />
-	<span class="toggle-label">Bluetooth</span>
-</label>
+<style lang="scss">
+	h1 {
+		display: flex;
+		align-items: center;
+		margin: 0;
+		font-size: 18px;
 
-<button>click</button>
+		img {
+			height: 16px;
+			margin-right: 8px;
+			color: white;
+		}
+	}
+
+	//
+
+	
+</style>
