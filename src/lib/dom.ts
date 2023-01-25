d77858b3a3fde00ca810705c511a843df6ff6fb4
crashdev226 <midashand44@gmail.com>
2023-01-25 23:54:32 +0200

    refactor: add constant for sticky bar

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -1,13 +1,13 @@
 const exists = (selector: string, element: Document | Element = document) =>
 	!!element.querySelector(selector);
 
-export const stickyBarExists = () => exists("[data-sticky-rating-bar]");
+const STICKY_BAR = "[data-sticky-rating-bar]";
+
+export const stickyBarExists = () => exists(STICKY_BAR);
 export const ratingBarExists = (parent: Element) => exists("[data-rating-bar]", parent);
 
 export function getStickyElements() {
-	const allElements = [
-		...(document.querySelectorAll("*:not([data-sticky-rating-bar]") as any),
-	];
+	const allElements = [...(document.querySelectorAll(`*:not(${STICKY_BAR})`) as any)];
 
 	const stickyElements = allElements.filter((el: HTMLElement) => {
 		const styles = getComputedStyle(el, null);
